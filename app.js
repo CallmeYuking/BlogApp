14ca40ddc1ba6bdadc672d5b8883294afd3335fe
Erica <ericaquan.edu@gmail.com>
2020-02-25 22:57:59 +0000

    add INDEX route and template

--- app.js
+++ app.js
@@ -18,15 +18,31 @@ var blogSchema = new mongoose.Schema({
 	created	: {type: Date, default: Date.now}
 })
 var Blog = mongoose.model("Blog", blogSchema);
+// Blog.create({
+// 	title: "Test Blog",
+// 	image: "https://images.unsplash.com/photo-1513754537542-f85cde07305a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+// 	body: "This is a blog post!!!"
+// })
 
 // RESTGUL ROUTES
 
-// title
-// image
-// body
-// created
+app.get("/", function(req, res){
+	res.redirect("/blogs")
+})
+
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.Blog(err)
+		} else {
+			res.render("index", {blogs: blogs});
+		}
+	})
+});
 
 
-app.listen(3000, function(){
+
+
+app.listen(5000, function(){
 	console.log("Server is running!!!")
 })
