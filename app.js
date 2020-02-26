6c41ded6f157227eff820becce240ad1f96ea1c7
Erica <ericaquan.edu@gmail.com>
2020-02-26 05:44:50 +0000

    add index.ejs to show the list of blog post. Add show.ejs to show more info to each blog post. add stylesheet and made navbar menu using senmantic UI.

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ var Blog = mongoose.model("Blog", blogSchema);
 // 	body: "This is a blog post!!!"
 // })
 
-// RESTGUL ROUTES
-
+// RESTFUL ROUTES
 app.get("/", function(req, res){
 	res.redirect("/blogs")
 })
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
 	Blog.find({}, function(err, blogs){
 		if(err){
@@ -40,7 +40,33 @@ app.get("/blogs", function(req, res){
 	})
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+	res.render("new")
+})
 
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+	//create blogs
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new");
+		} else {
+			res.redirect("/blogs")
+		}
+	})
+})
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("show", {blog: foundBlog})
+		}
+	})
+})
 
 
 app.listen(5000, function(){
